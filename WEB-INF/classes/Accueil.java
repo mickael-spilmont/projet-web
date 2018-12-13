cdffeb8df5861d2a4c42d9933458be28ffd51a30
Mickael <mickael.spilmont.fr@gmail.com>
2018-12-13 17:25:48 +0100

    Creation de la servlet de login (non encore fini)

--- WEB-INF/classes/Accueil.java
+++ WEB-INF/classes/Accueil.java
@@ -11,10 +11,10 @@ public class Accueil extends HttpServlet {
   public void service(HttpServletRequest req,HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/html");
     resp.setCharacterEncoding("UTF-8");
-
     PrintWriter out = resp.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
+
+    out.print("<!DOCTYPE html>");
+    out.print("<html>");
     out.println("<head>");
     out.println("<meta charset=\"utf-8\" />");
     out.println("<title>Accueil</title>");
@@ -32,7 +32,8 @@ public class Accueil extends HttpServlet {
     try {
       // On déclare le type de driver JDBC et le chemin d’accès à la base, si pb exception ClassNotFound
       Class.forName("org.sqlite.JDBC");
-      String dbURL =  "jdbc:sqlite:/home/infoetu/spilmonm/TPs/Prog Web/tomcat8/webapps/projet/BDD/base.db";
+      // String dbURL =  "jdbc:sqlite:/home/infoetu/spilmonm/TPs/Prog Web/tomcat8/webapps/projet/BDD/base.db";
+      String dbURL =  "jdbc:sqlite:../../BDD/base.db";
 
       //On essaye de se connecter à la base
       Connection conn = DriverManager.getConnection(dbURL);
@@ -55,7 +56,7 @@ public class Accueil extends HttpServlet {
           // int rangJava = rs.getInt("rang");
           // String passwordJava = rs.getString("password");
 
-          out.println(nomJava + " " + dateJava);
+          out.println(nomJava + " " + dateJava + "</br>");
         }
 
         // On ferme les connexions au ResultSet, Statement et à la base
--- WEB-INF/classes/Connexion.java
+++ WEB-INF/classes/Connexion.java
@@ -3,9 +3,51 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.sql.*;
+import java.time.*;
 
-@WebServlet("/verif-login")
+@WebServlet("/connexion")
 public class Connexion extends HttpServlet {
   public void service(HttpServletRequest req,HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding("UTF-8");
+    PrintWriter out = resp.getWriter();
 
+    String mail = req.getParameter("mail");
+    String password = req.getParameter("password");
+
+    HttpSession session = req.getSession();
+
+    try {
+      Class.forName("org.sqlite.JDBC");
+      String dbURL =  "jdbc:sqlite:../../BDD/base.db";
+      Connection conn = DriverManager.getConnection(dbURL);
+
+      if (conn != null) {
+        Statement stat = conn.createStatement();
+        String requette = "SELECT id, rang FROM utilisateur "
+          + "WHERE mail = '" + mail + "' AND password = '" + password + "';";
+
+        ResultSet rs = stat.executeQuery(requette);
+
+        while (rs.next()) {
+          int idJava =  rs.getInt("id");
+          int rangJava = rs.getInt("rang");
+
+          out.print(idJava + "</br>" + rangJava + "</br>");
+        }
+        rs.close();
+        stat.close();
+        conn.close();
+      }
+    }
+    catch (ClassNotFoundException ex) {
+      ex.printStackTrace();
+      out.println("ClassNotFoundException");
+    }
+    catch (SQLException ex) {
+      ex.printStackTrace();
+      out.println("SQLException");
+    }
   }
+}
