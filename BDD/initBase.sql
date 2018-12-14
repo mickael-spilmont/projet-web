ae84ad3c4d52837f05445069d03969e7bc913c4f
alexandre-duhamel <alexandreduhamel59@gmail.com>
2018-12-14 13:55:45 +0100

    correction initBase mal placée

--- BDD/initBase.sql
+++ BDD/initBase.sql
@@ -1,4 +1,3 @@
-
 drop table if exists utilisateur;
 drop table if exists livre;
 drop table if exists exemplaire;
@@ -44,14 +43,14 @@ create table demande(
 	date_demande datetime default current_timestamp
 );
 
--- CrÃ©ation des status
+-- Création des status
 insert into status (type) values
-	('PrÃªt'),
+	('Prêt'),
 	('Echange'),
 	('Don'),
 	('En cour d''emprunt');
 
--- CrÃ©ation d''utilisateurs, les deux premier sont admin
+-- Création d''utilisateurs, les deux premier sont admin
 insert into utilisateur (prenom, nom, date_naissance, mail, rang, password) values
 	('Mickael', 'Spilmont', '1987-09-12', 'mickael.spilmont@webmail.com', 10, '0000'),
 	('Alexandre', 'Duhamel', '1991-12-09', 'alexandre.duhamel@webmail.com', 10, '0000'),
@@ -59,15 +58,15 @@ insert into utilisateur (prenom, nom, date_naissance, mail, rang, password) valu
 	('leila', 'talica', '1950-10-28', 'leila.talica@webmail.com', 1, '0000'),
 	('Alice', 'Machin', '2001-07-18', 'alice.talica@webmail.com', 1, '0000');
 
--- CrÃ©ation de Livres
+-- Création de Livres
 insert into livre (titre, auteur, editeur, isbn) values
-	('Harry Potter Ã  l''Ecole des Sorciers', 'J.K.Rowling', 'Gallimard', '2070643026'),
-	('Zothique', 'Clark Ashton Smith', 'MnÃ©mos', '2354085885'),
-	('Averoigne', 'Clark Ashton Smith', 'MnÃ©mos', '2354086105'),
-	('Les ContrÃ©es du rÃªve', 'Howard Phillips Lovecraft', 'Bragelone', '9791028110994'),
-	('La QuÃªte Onirique de Kadath l''Inconnue', 'Howard Phillips Lovecraft', 'Bragelone', 'B073SDW4VV');
+	('Harry Potter à l''Ecole des Sorciers', 'J.K.Rowling', 'Gallimard', '2070643026'),
+	('Zothique', 'Clark Ashton Smith', 'Mnémos', '2354085885'),
+	('Averoigne', 'Clark Ashton Smith', 'Mnémos', '2354086105'),
+	('Les Contrées du rêve', 'Howard Phillips Lovecraft', 'Bragelone', '9791028110994'),
+	('La Quête Onirique de Kadath l''Inconnue', 'Howard Phillips Lovecraft', 'Bragelone', 'B073SDW4VV');
 
--- CrÃ©ation d'exemplaire
+-- Création d'exemplaire
 insert into exemplaire (id_util, id_livre, id_status) values
 	(3, 2, 1),
 	(3, 3, 2),
@@ -76,26 +75,38 @@ insert into exemplaire (id_util, id_livre, id_status) values
 	(4, 4, 1),
 	(5, 4, 1),
 	(5, 5, 1);
-
+ 
 -- Quelque test d'affichage
 SELECT prenom, nom, date_naissance, mail FROM utilisateur;
 SELECT titre, auteur, editeur, isbn FROM livre;
 
 -- Login
 SELECT prenom, nom FROM utilisateur
-WHERE mail = 'mickael.spilmont@webmail.com' AND password = '0000';
+    WHERE mail = 'mickael.spilmont@webmail.com' AND password = '0000';
 
--- Afficher les 5 dernier exemplaires ajoutÃ©s, avec la date, le titre, l'auteur,
--- l'utilisateur qui le possÃ¨de et le id_status, tu trouveras un dÃ©but ci-dessous
--- mais le rÃ©sultat n'est pas complet
+-- Afficher les 5 dernier exemplaires ajoutés, avec la date, le titre, l'auteur,
+-- l'utilisateur qui le possède et le id_status, 
+SELECT titre, auteur, date_exemplaire, type -- je prend ici le type plutot que le id_status, c'est plus lisible
+from exemplaire INNER JOIN livre ON livre.id = exemplaire.id_livre
+                INNER JOIN utilisateur ON exemplaire.id_util = utilisateur.id
+                INNER JOIN status ON status.id = exemplaire.id_status
+    order by date_exemplaire desc limit 3;
 
--- SELECT exemplaire.date_exemplaire, livre.titre, livre.auteur, livre.editeur, utilisateur.prenom, utilisateur.nom, status.type FROM exemplaire
--- INNER JOIN utilisateur ON utilisateur.id = exemplaire.id_util
--- INNER JOIN livre ON livre.id = exemplaire.id_livre
--- INNER JOIN status ON status.id = exemplaire.id_exemplaire;
+-- Afficher tout les livres possédés par un utilisateur (titre, auteur, editeur, isbn, status)
+SELECT titre, auteur, editeur, isbn, type 
+from livre INNER JOIN exemplaire ON livre.id = exemplaire.id_livre
+           INNER JOIN utilisateur ON exemplaire.id_util = utilisateur.id
+           INNER JOIN status ON status.id = exemplaire.id_status
+   WHERE utilisateur.mail='leila.talica@webmail.com'; -- remplace l'adresse par ton parametre
 
--- Afficher tout les livres possÃ©dÃ©s par un utilisateur (titre, auteur, editeur, isbn, status)
-
+SELECT * FROM exemplaire;
 -- Updater le status d'un livres
+UPDATE exemplaire 
+    SET id_status = (SELECT id FROM status WHERE type='Don') -- remplace 'Don' par le parametre
+    WHERE id_exemplaire=2; -- remplace 2 par le parametre
 
 -- suprimer un livre des exemplaires, mais pas de la table livre
+DELETE FROM exemplaire
+    WHERE id_exemplaire=3;  -- remplace 3 par ton parametre
+    
+SELECT * FROM exemplaire;
